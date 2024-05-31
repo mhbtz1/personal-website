7d2fdf0b5b62632d5c12175ac3c202c5db440147
Matthew Habtezgi <mhbtz@mit.edu>
2024-05-30 21:28:21 -0400

    color yellow

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,7 @@
 import "../css/Sidebar.css"
 import { Button } from '@mui/material'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { lime, purple, yellow } from '@mui/material/colors';
 
 const endpoint = "https://habtem.onrender.com/resume"
 const aboutEndpoint = "https://habtem.onrender.com/about"       
@@ -20,12 +22,12 @@ function Sidebar() {
                     <img src="/IMG_1295.jpg" alt="Profile" />
                 </div>
                 <ul>
-                    <li> <Button color="white" variant="contained" href={aboutEndpoint} style={{'width' : '200px', 'height': '20px'}}> <p style={listStyles}> About </p> </Button> </li>
-                    <li> <Button color="white" variant="contained" style={{'width': '200px', 'height' : '20px'}}> <p style={listStyles}> Technical Articles </p> </Button> </li>
-                    <li> <Button color="white" variant="contained" style={{'width' : '200px', 'height': '20px'}}> <p style={listStyles}> Projects</p> </Button> </li>
-                    <li> <Button color="white" variant="contained" style={{'width' : '200px', 'height' : '20px'}}> <p style={listStyles}> Upwork </p> </Button> </li>
-                    <li> <Button color="white" variant="contained" style={{'width' : '200px', 'height' : '20px'}}> <p style={listStyles}> Other </p> </Button> </li>
-                    <li> <Button color="white" variant="contained" href={endpoint} style={{'width' : '200px', 'height' : '20px'}}> <p style={listStyles}> Resume </p> </Button> </li>
+                    <li> <Button color={yellow} variant="contained" href={aboutEndpoint} style={{'width' : '200px', 'height': '20px'}}> <p style={listStyles}> About </p> </Button> </li>
+                    <li> <Button color={yellow} variant="contained" style={{'width': '200px', 'height' : '20px'}}> <p style={listStyles}> Technical Articles </p> </Button> </li>
+                    <li> <Button color={yellow} variant="contained" style={{'width' : '200px', 'height': '20px'}}> <p style={listStyles}> Projects</p> </Button> </li>
+                    <li> <Button color={yellow} variant="contained" style={{'width' : '200px', 'height' : '20px'}}> <p style={listStyles}> Upwork </p> </Button> </li>
+                    <li> <Button color={yellow} variant="contained" style={{'width' : '200px', 'height' : '20px'}}> <p style={listStyles}> Other </p> </Button> </li>
+                    <li> <Button color={yellow} variant="contained" href={endpoint} style={{'width' : '200px', 'height' : '20px'}}> <p style={listStyles}> Resume </p> </Button> </li>
                 </ul>
             </div>
         </>
