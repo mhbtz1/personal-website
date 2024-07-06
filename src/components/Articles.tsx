307d976a30c0e77ffc7f42b62aa25a33cf812e01
Matthew Habtezgi <mhbtz@mit.edu>
2024-07-06 03:01:19 -0400

    add Articles

---
+++ src/components/Articles.tsx
@@ -0,0 +1,15 @@
+function Article() {
+    
+    return (<>
+        <div className="background-color: #999">
+            <h3 className="vertical-align: middle"> Some writings on things I have learned / built. </h3>
+            <ul>
+                <li className="vertical-align: middle"> 1. <a href="http://habtem.onrender.com/article/1"> Semper Incompletum </a> </li>
+                <li className="vertical-align: middle"> 2. <a href="http://habtem.onrender.com/article/2"> Hic Etiam </a> </li>
+            </ul>
+        </div>
+    </>)
+}
+
+
+export default Article;
