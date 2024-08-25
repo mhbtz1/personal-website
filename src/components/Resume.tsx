ac511053d0e5964f440f3fc13cc38707c1b981d2
Matthew Habtezgi <mhbtz@mit.edu>
2024-08-25 15:33:09 -0400

    oops

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -15,7 +15,7 @@ function Resume() {
     */
 
     return  <>
-             <iframe src='/MMH_082424.pdf' width="100%" height="100%"> </iframe>
+             <iframe src='/MMH-082424.pdf' width="100%" height="100%"> </iframe>
             </>
 }
 
