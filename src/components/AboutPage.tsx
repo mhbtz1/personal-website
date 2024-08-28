542b847c3d25599fd18369cc5087e4d593fc98fb
Matthew Habtezgi <mhbtz@mit.edu>
2024-08-27 22:24:50 -0400

    ship

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -8,7 +8,6 @@ export function AboutPage() {
             <p> Hello, I am Matthew Habtezgi. I am a student in university studying Computer Science and Mathematics.</p>
             <p> I am generally interested in software engineering, ML / formal methods research, and low-level systems.</p>
         </div>
-    
     </>
 }
 export default AboutPage
