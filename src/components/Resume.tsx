21ecac143620ef8055b24b8c43d68dd41e805e9c
Matthew Habtezgi <mhbtz@mit.edu>
2024-08-30 16:31:24 -0400

    ship

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -3,7 +3,7 @@ console.log("Running Resume.tsx")
 
 function Resume() {
     return  <div className="resume-inner">
-             <iframe src='./public/MMH-082424.pdf' width="100%" height="100%"> </iframe>
+             <iframe src='./MMH-082424.pdf' width="100%" height="100%"> </iframe>
             </div>
 }
 
