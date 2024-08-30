3fbb2b92d8607aef706738e254d01eb19ca09f41
Matthew Habtezgi <mhbtz@mit.edu>
2024-08-30 15:59:56 -0400

    resume -> resume-inner

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -2,7 +2,7 @@ import '../css/Resume.css'
 console.log("Running Resume.tsx")
 
 function Resume() {
-    return  <div className="resume">
+    return  <div className="resume-inner">
              <iframe src='./public/MMH-082424.pdf' width="100%" height="100%"> </iframe>
             </div>
 }
