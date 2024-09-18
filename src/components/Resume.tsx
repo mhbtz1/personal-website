43588e306be5e0a2b731a328110c0e0a1763b9f8
Matthew Habtezgi <mhbtz@mit.edu>
2024-09-17 22:18:15 -0400

    update resume

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -3,7 +3,7 @@ console.log("Running Resume.tsx")
 
 function Resume() {
     return  <>
-             <iframe src='./MMH_082424.pdf' width="100%" height="100%"> </iframe>
+             <iframe src='./MMH_09162024.pdf' width="100%" height="100%"> </iframe>
             </>
 }
 
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,54 +1,3 @@
-/*
-import "../css/Sidebar.css"
-import { Button } from '@mui/material'
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { lime, purple, yellow } from '@mui/material/colors';
-
-const endpoint = "https://habtem.onrender.com/resume"
-const aboutEndpoint = "https://habtem.onrender.com/about"   
-const technicalEndpoint = "http://habtem.onrender.com/articles"    
-
-function Sidebar() {
-
-    console.log("resume endpoint: ", endpoint)
-    console.log("about endpoint: ", aboutEndpoint)
-
-    const listStyles = {
-        'font-size' : '13px',
-    }
-
-    const theme = createTheme({
-        palette: {
-          primary: { main: '#FFFFFF' },
-          secondary: { main: '#FFFFFF' }
-        },
-    });
-
-    return <>
-            <div className="sidebar">
-                <h2>Matthew Habtezgi</h2>
-                <div className="sidebar-image">
-                    <img src="/IMG_1295.jpg" alt="Profile" />
-                </div>
-                <ul>
-                    <ThemeProvider theme={theme}>
-                        <li> <Button variant="contained" href={aboutEndpoint} style={{'width' : '12vw', 'height': '1.25vw'}}> <p style={listStyles}> About </p> </Button> </li>
-                        <li> <Button variant="contained" href={technicalEndpoint} style={{'width': '12vw', 'height' : '1.25vw'}}> <p style={listStyles}> Technical Articles </p> </Button> </li>
-                        <li> <Button variant="contained" style={{'width' : '12vw', 'height': '1.25vw'}}> <p style={listStyles}> Projects</p> </Button> </li>
-                        <li> <Button variant="contained" style={{'width' : '12vw', 'height' : '1.25vw'}}> <p style={listStyles}> Upwork </p> </Button> </li>
-                        <li> <Button variant="contained" style={{'width' : '12vw', 'height' : '1.25vw'}}> <p style={listStyles}> Other </p> </Button> </li>
-                        <li> <Button variant="contained" href={endpoint} style={{'width' : '12vw', 'height' : '1.25vw'}}> <p style={listStyles}> Resume </p> </Button> </li>
-                    </ThemeProvider>
-                </ul>
-            </div>
-        </>
-
-}
-
-export default Sidebar;
-*/
-
-
 import React from 'react';
 import { Button } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -56,6 +5,8 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 const endpoint = "https://habtem.onrender.com/resume";
 const aboutEndpoint = "https://habtem.onrender.com/about";
 const technicalEndpoint = "http://habtem.onrender.com/articles";
+const projectEndpoint = "https://habtem.onender.com/projects"
+const miscellaneousEndpoint = "https://habtem.onrender.com/miscellaneous"
 
 function Sidebar() {
   const theme = createTheme({
@@ -69,15 +20,14 @@ function Sidebar() {
     <div className="sidebar">
       <h2>Matthew Habtezgi</h2>
       <div className="sidebar-image">
-        <img src="/IMG_1295.jpg" alt="Profile" />
+        <img src="./IMG_1295.jpg" alt="Profile" />
       </div>
       <ThemeProvider theme={theme}>
         <nav>
           <Button variant="contained" href={aboutEndpoint}>About</Button>
           <Button variant="contained" href={technicalEndpoint}>Technical Articles</Button>
-          <Button variant="contained">Projects</Button>
-          <Button variant="contained">Upwork</Button>
-          <Button variant="contained">Other</Button>
+          <Button variant="contained" href={projectEndpoint}>Projects</Button>
+          <Button variant="contained" href={miscellaneousEndpoint}>Miscellaneous</Button>
           <Button variant="contained" href={endpoint}>Resume</Button>
         </nav>
       </ThemeProvider>
