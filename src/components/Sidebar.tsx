8f4b0fd6da203e181cd12a98484b328a9e843c2b
Matthew Habtezgi <matt@habtem.dev>
2025-12-04 22:44:29 -0800

    image optimization

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,6 +7,7 @@ import Miscellaneous from './Miscellaneous';
 import Resume from './Resume';
 import LinkedIn from './LinkedIn';
 import '../css/Sidebar.css';
+import { Image } from 'next/image';
 
 function SidebarContent() {
   const location = useLocation();
@@ -80,7 +81,7 @@ function SidebarContent() {
         </div>
         
         <div className="sidebar-image-container">
-          <img
+          <Image
             src="/pfp-revised.jpg"
             alt="Matthew Habtezgi"
             className="sidebar-image"
