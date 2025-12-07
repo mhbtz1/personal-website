809233aed4be464cfca654eaea9f0db5f7c6892d
Matthew Habtezgi <matt@habtem.dev>
2025-12-07 01:31:16 -0800

    fix articles

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -2,8 +2,10 @@ import '../css/Articles.css'
 
 function Article() {
     const articles = [
-        { id: 1, title: 'Semper Incompletum' },
-        { id: 2, title: 'Hic Etiam' }
+        { id: 1, title: 'Primitives for Agentic Applications', link: 'https://www.linkedin.com/feed/update/urn:li:activity:7398482291158634496/?originTrackingId=SHm21Rny7wIt5pu%2FWeWhfw%3D%3D'},
+        { id: 2, title: 'Evaluating Agents for Critical Applications', link: 'https://www.linkedin.com/feed/update/urn:li:activity:7395151671783301120/?originTrackingId=dLK%2FXFckQLLUfh554ZOQJg%3D%3D'},
+        { id: 3, title: 'Agent Design Thoughts', link: 'https://www.linkedin.com/feed/update/urn:li:activity:7391927297546657792/?originTrackingId=2aM7umTEOiLG%2FYktqSdJTw%3D%3D'},
+        { id: 4, title: 'UX For AI Applications', link: 'https://www.linkedin.com/feed/update/urn:li:activity:7389351368324710400/?originTrackingId=zW3bPtICZbFn2Eimun8tjg%3D%3D'}
     ];
 
     return (
@@ -14,7 +16,9 @@ function Article() {
                     <li key={article.id} className="article-item">
                         <span className="article-number">{index + 1}</span>
                         <a 
-                            href={`${import.meta.env.ACTIVE_HOST}/article/${article.id}`}
+                            href={article.link}
+                            target="_blank"
+                            rel="noopener noreferrer"
                             className="article-link"
                         >
                             {article.title}
